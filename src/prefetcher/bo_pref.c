50c5cbbd6a7f5b19cd55158e5441c2b0fa3dd638
Ella Lehavi <elehavi@ucsc.edu>
2024-11-18 10:54:51 -0800

    added files for final prefetcher

---
+++ src/prefetcher/bo_pref.c
@@ -0,0 +1,124 @@
+/***************************************************************************************
+ * File         : bo_pref.c
+ * Author       : Ella and Rushali
+ * Date         : November 2024
+ * Description  : Best Offset Prefetcher (https://ieeexplore.ieee.org/document/7446087)
+ ***************************************************************************************/
+
+#include "debug/debug_macros.h"
+#include "debug/debug_print.h"
+#include "globals/global_defs.h"
+#include "globals/global_types.h"
+#include "globals/global_vars.h"
+
+#include "globals/assert.h"
+#include "globals/utils.h"
+#include "op.h"
+
+#include "core.param.h"
+#include "debug/debug.param.h"
+#include "general.param.h"
+#include "libs/hash_lib.h"
+#include "libs/list_lib.h"
+#include "memory/memory.param.h"
+#include "prefetcher//bo_pref.h"
+// #include "prefetcher//pref_stride.param.h"
+#include "prefetcher/pref.param.h"
+#include "prefetcher/pref_common.h"
+#include "statistics.h"
+
+/**************************************************************************************/
+/* Global Variables */
+
+extern Memory*       mem;
+extern Dcache_Stage* dc;
+static Cache*        l1_cache;
+
+/***************************************************************************************/
+/* Local Prototypes */
+
+int ROUNDMAX;
+int SCOREMAX;
+int i; // loop variable
+
+Hash_Table rr_table;  
+Hash_Table score_table;
+static const int* const offset_list[] = {1, 2, 3, 4, 5, 6, 8, 9, 
+                                        10, 12, 15, 16, 18, 20, 24, 
+                                        25, 27, 30, 32, 36, 40, 45,
+                                        48, 50, 54, 60, 64, 72, 75,
+                                        80, 81, 90, 96, 100, 108, 120,
+                                        125, 128, 135, 144, 150, 160, 
+                                        162, 180, 192, 200, 216, 225,
+                                        240, 243, 250, 256 };
+
+
+/*********************************************************************/
+
+void init_prefetch(void) {
+    // TODO: what goes here?
+    //  maybe hwp stuff...
+}
+
+void bo_pref_init(void) {
+    // TODO: initialize data structures
+    // rr table
+    init_hash_table(&rr_table, "RR Table", 
+                    256 /*TODO: how many buckets?*/, 
+                    4 /*TODO: what data size?*/);
+    // score table
+    init_hash_table(&score_table, "Score Table", 51, sizeof(int) );
+
+    ROUNDMAX = 1;
+    SCOREMAX = 15;
+}
+
+
+/*********************************************************************/
+
+
+/*TODO: prefetch the actual data.*/
+ // How/should it b integrated into learning phase?
+ // New_mem_req API lets you request memory. Always provide callback fcn!
+
+/*
+Consists of several rounds. 
+At the start, the score is set to 0.
+On every L2 miss/prefetch hit, test an offset d_i from the list
+If X - d_i hits in the recent requests table, increment its score. 
+Each offset is tested like this once per round. 
+Learning phase ends when either…
+One of the scores hits the maximum, or the number of rounds hits the maximum. 
+We then find the offset with the highest score. This becomes the new offset.
+New learning phase starts
+*/
+void learning_phase(Hash_Table* rr_table, Hash_Table* score_table) {
+    int scoremax_reached;
+    int optimal_offset;
+    // TODO: set values in score table to 0. memset?
+    // Check for prefetch miss/hit
+    // TODO: update RR table whenever a prefetch request is made
+    for (i = 0; i < ROUNDMAX; i++) {
+        // TODO: is each round 1 access
+        // for each offset
+            // test prefetching from the offset
+            // if it hits in RR table, score +=1
+            // if score max, break out of both loops.
+            if (score == SCOREMAX) {
+                scoremax_reached = 1;
+                optimal_offset = offset;
+                break;
+                // TODO: could we return?
+            }
+        if (scoremax_reached) {
+            break;
+        }  
+
+    }
+
+    // TODO: set best offset to the prefetch offset
+    // TODO: tie?
+    /* Learning phase ends when:
+    one of the scores equals SCOREMAX, 
+    the number of rounds equals ROUNDMAX (a fixed parameter).*/
+}
---
+++ src/prefetcher/bo_pref.h
@@ -0,0 +1,52 @@
+/***************************************************************************************
+ * File         : l2l1pref.h
+ * Author       : HPS Research Group
+ * Date         : 03/24/2004
+ * Description  : L2 to L1 prefetcher support functions
+ ***************************************************************************************/
+
+#ifdnef __BO_PREF_H__
+#define __BO_PREF_H__
+
+#include "dcache_stage.h"
+#include "globals/global_types.h"
+#include "libs/hash_lib.h"
+#include "pref_type.h"
+
+/**************************************************************************************/
+/* Global vars */
+
+
+/**************************************************************************************/
+/* Local vars */
+
+/**************************************************************************************/
+/* Data Structures */
+
+/* Struct RR table*/
+
+/* Offset list*/
+
+/* score table */
+// typedef struct Score_table_entry_struct {
+//     int offset;
+//     int score;
+// } Score_Table_Entry;
+
+// typedef struct Score_table_struct {
+//     uns num_offsets;
+
+// } Score_Table;
+
+//TODO: separately defined data structures?
+
+/**************************************************************************************/
+/* Functions/Routines */
+
+// TODO: What is HWP? 
+void init_prefetch(void);
+void bo_pref_init(void /* HWP* hwp */);
+
+
+
+#endif
